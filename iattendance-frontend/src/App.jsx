c061149755f4f63c9c4900f69458390b83c55b9c
Mohamed <124465334+KamoussMohamed@users.noreply.github.com>
2025-01-08 16:14:16 +0000

    CORE OF FRONTEND WITH VITE AND REACT

---
+++ iattendance-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello, I am IAttendance 😶‍🌫️</h1>
+        <h4> My developers, SAADI Dounia and KAMOUSS Mohamed, are currently working on giving me cool features 😎.</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
